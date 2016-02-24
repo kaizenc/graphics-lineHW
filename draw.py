9a7bccad620afa2c18ee0f2ebe6019592887fba1
Kaizen <imgtn159@gmail.com>
2016-02-23 20:53:17 -0500

    Finished

--- draw.py
+++ draw.py
@@ -5,55 +5,45 @@ def draw_line( screen, x0, y0, x1, y1, color ):
     if x1 < x0:
         x0, x1 = x1, x0
         y0, y1 = y1, y0 
-    if (y1 >= y0): #if positive slope
-        dx = x1 - x0
-        dy = y1 - y0
-        if y1 - y0 > x1 - x0: #if slope > 1
-            k = (dx * 2) - dy
-            x = x0
-            for y in range (y0, y1 + 1):
-                if (k < 0):
-                    k += (dx * 2)
-                    plot(screen, color, x, y)
-                else:
-                    k += 2 * (dx - dy)
-                    x += 1
-                    plot(screen, color, x, y)
-        else: #if slope <= 1
-            k = (dy * 2) - dx
-            y = y0
-            for x in range (x0, x1 + 1):
-                if (k < 0):
-                    k += (dy * 2)
-                    plot(screen, color, x, y)
-                else:
-                    k += 2 * (dy - dx)
+        
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1
+
+    if y1 > y0:
+        d = 2 * A + B
+        if -1 * B > A:
+            while x < x1:  # Octant 1 / 5
+                plot(screen, color, x, y)
+                if d > 0:
                     y += 1
-                    plot(screen, color, x, y)
-    else: #negative slope
-        y0, y1 = -1*y0, -1*y1
-        dx = x1 - x0
-        dy = y1 - y0
-        if y0 - y1 > x1 - x0: #if slope > -1
-            k = (dx * 2) - dy
-            x = x0
-            for y in range (y0, y1 + 1):
-                if (k < 0):
-                    k += (dx * 2)
-                    plot(screen, color, x, -1*y)
-                else:
-                    k += 2 * (dx - dy)
+                    d += 2 * B
+                x += 1
+                d += 2 * A
+        else:
+            while y < y1:  # Octant 2 / 6
+                plot(screen, color, x, y)
+                if d < 0:
                     x += 1
-                    plot(screen, color, x, -1*y)
-        else: #if slope < -1
-            k = (dy * 2) - dx
-            y = y0
-            for x in range (x0, x1 + 1):
-                if (k < 0):
-                    k += dy * 2
-                    plot(screen, color, x, -1*y)
-                else:
-                    k += 2 * (dy - dx)
-                    y += 1
-                    plot(screen, color, x, -1*y)
-    save_ppm( screen, "Test1.ppm" )
+                    d += 2 * A
+                y += 1
+                d += 2 * B
+    else:
+        d = 2 * A - B
+        if A > B:
+            while x < x1:  # Octant 4 / 8
+                plot(screen, color, x, y)
+                if d < 0:
+                    y -= 1
+                    d -= 2 * B
+                x += 1
+                d += 2 * A
+        else:
+            while y > y1:  # Octant 3 / 7
+                plot(screen, color, x, y)
+                if d > 0:
+                    x += 1
+                    d += 2 * A
+                y -= 1
+                d -= 2 * B
